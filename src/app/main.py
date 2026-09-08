208e8ca1339a723f368f7185dcc227e629314195
hamasli <hamasali534@gmail.com>
2026-09-08 13:41:35 +0200

    Deploy frontend with docker

--- src/app/main.py
+++ src/app/main.py
@@ -10,7 +10,7 @@ from src.app.api.routes.refunds import router as refund_router;
 from src.app.api.routes.conversations import (
     router as conversations_router,
 )
-import os;
+
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -38,9 +38,14 @@ origins = os.getenv(
     "CORS_ORIGINS",
     "http://localhost:5173,http://127.0.0.1:5173"
 ).split(",")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://98.88.84.129",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
